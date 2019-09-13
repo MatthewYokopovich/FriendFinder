76b9917fc618242448bd6aa876043bf312521bf8
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-09-13 16:07:11 -0500

    adjusted htmlRoutes for url parsing

--- server.js
+++ server.js
@@ -3,3 +3,19 @@ var path = require("path");
 
 var app = express();
 var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var friends = require("./app/data/friends");
+
+var htmlRoutes = require("./app/routing/htmlRoutes")(app, path);
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+  });
+  
