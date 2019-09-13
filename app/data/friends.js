9000d1d05807dbef1bffacdcda1e198e7532b5f4
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-09-13 14:33:24 -0500

    initialized most files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+      "name": "Yoda",
+      "photo": "/../img/yoda.png",
+      "scores": [
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+      ]
+    }
+  ];
+
+  module.exports = friends;
